1a46430b2103677da3bd601913bbb1726a218c78
Pallava-Joshi <joshipallava@gmail.com>
2025-08-13 12:50:27 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-amber-500'>
+        Hello there!
+       </div> 
+    </>
+  )
+}
+
+export default App
